7f7a645f728cfa1f49f0bbb1830720f9aa04044a
Philip Yurchuk <philip.yurchuk@madeupname.com>
2021-12-03 21:00:41 -0800

    Followed Amplify Next.js tutorial. Added TypeScript support.

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "tsnextamplified": {
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "auth": {
+        "tsnextamplifiedc52d3f3f": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
